0339e0c68983f60380d34661e1d6be10ec7e2a32
dezzoko <dezzoko@mail.ru>
2023-08-15 20:57:49 +0300

    init router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import { Counter } from "./components/Counter";
+import React, { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 export const App = () => {
   return (
     <div>
-      <Counter />
+      <Link to={"about"}>About</Link>
+      <Link to={""}>Home</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageLazy />} />
+          <Route path={"/"} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "../../components/Counter";
+
+const MainPage = () => {
+  return (
+    <div>
+      MainPage
+      <Counter />
+    </div>
+  );
+};
+
+export default MainPage;
