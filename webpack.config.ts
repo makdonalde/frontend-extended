23556adf5f941fdc7e253a240337dc905bbaa8df
dezzoko <dezzoko@mail.ru>
2023-08-15 23:37:44 +0300

    fix file structure

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import webpack from "webpack";
-import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildEnv, BuildPath } from "./config/build/types/config";
+import { buildWebpackConfig } from "./config/buildWebpackConfig";
+import { BuildEnv, BuildPath } from "./config/types/config";
 
 export default (env: BuildEnv) => {
   const paths: BuildPath = {
