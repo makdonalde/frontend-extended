72cae3ac07ad591030ca2e4ed506a8dd1ff50002
dezzoko <dezzoko@mail.ru>
2023-09-21 08:47:33 +0200

    add tests for thunk

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSliceTest', () => {
+    test('test set username', () => {
+        const state:DeepPartial<LoginSchema> = { username: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('12345'))).toEqual({
+            username: '12345',
+        });
+    });
+    test('test set password', () => {
+        const state:DeepPartial<LoginSchema> = { password: '123kkk' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('123'))).toEqual({
+            password: '123',
+        });
+    });
+});
